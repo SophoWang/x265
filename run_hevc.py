81d5114601e846dd2a1b8b5632d27b88bea3ea19
sopho <50799755+SophoWang@users.noreply.github.com>
2026-07-20 10:28:20 +0800

    更新 run_hevc.py

--- run_hevc.py
+++ run_hevc.py
@@ -9,10 +9,10 @@ json_str = ""
 
 with open(vscode_launch, "r") as fd:
     lines = fd.readlines()
-				# data = json.load(fd)
-				for line in lines:
-								line = re.sub(r'//.*', "", line)
-								json_str = json_str + line
+    # data = json.load(fd)
+    for line in lines:
+        line = re.sub(r'//.*', "", line)
+        json_str = json_str + line
 
 print(json_str)
 
@@ -23,7 +23,7 @@ run_args = ""
 run_cmd = "./build/x265"
 
 for arg in args:
-				run_args = run_args + " " + arg
+    run_args = run_args + " " + arg
 
 print(run_cmd + run_args)
 os.system(run_cmd + run_args)
